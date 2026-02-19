e326fae240acb3a6f763d69c2522206df66b3219
Matthias3721 <Matt.w00@wp.pl>
2026-02-19 14:47:17 +0100

    Fix CI: run test with H2 test profile

--- backend/src/test/java/com/matthias/dealmonitor/SmokeTest.java
+++ backend/src/test/java/com/matthias/dealmonitor/SmokeTest.java
@@ -2,8 +2,10 @@ package com.matthias.dealmonitor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SmokeTest {
     @Test void contextLoads() {}
 }
